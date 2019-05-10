b51eea516a6aaa43f772e721a79db2854d9cedf6
yerin <hepari2154@naver.com>
2019-05-10 23:42:07 +0900

    first commit

---
+++ src/main/java/com/example/zepzep/ZepzepApplication.java
@@ -0,0 +1,13 @@
+package com.example.zepzep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZepzepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZepzepApplication.class, args);
+    }
+
+}
